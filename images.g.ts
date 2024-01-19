5d6b6adb580ad646da3feab8667068bcd151e3e8
Guigui421 <156448624+Guigui421@users.noreply.github.com>
2024-01-19 13:26:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Nau":return img`
+. . . . . . . f f . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f f . . 
+. f f f f . . . . . . f f f f . 
+. 2 f f 2 . . . . . . 2 f f 2 . 
+. 2 5 4 5 . . . . . . 4 5 4 2 . 
+. 4 4 5 2 . . . . . . 2 4 5 5 . 
+. . 5 4 . . . . . . . . 2 4 . . 
+`;
+            case "image2":
+            case "Meteorit":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . b b b b . . . 
+. . . . . . . . b b b b b c . . 
+. . . . . . . . b c b b b c b . 
+. . . . . . . b b c c b b c b . 
+. . . . . . . b b b b b b b b . 
+. . . . . . b b b b b b b b b . 
+. . . . b b b b b b b b b b b . 
+. . . b b c b b b b c b b c b . 
+. . b b b c c b b c c b b b . . 
+. b b b b b b b b b b b b . . . 
+. b b b b b b b b b b . . . . . 
+b c c b b b b b b b b . . . . . 
+b c c b b b b c b b . . . . . . 
+. b b b c b b c b . . . . . . . 
+. . . b b b . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
